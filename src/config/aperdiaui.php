4b7c0ba793a2cbf5e94a5bb8f91f35eee268aeba
caouecs <caouecs@caouecs.net>
2016-10-03 22:56:12 +0200

    alux

--- src/config/aperdiaui.php
+++ src/config/aperdiaui.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'style' => 'alux',
+    'style' => 'bootstrap3',
 ];
--- src/views/alux/form/inputGroup.blade.php
+++ src/views/alux/form/inputGroup.blade.php
@@ -1,13 +1,14 @@
+<div>
 @if ($label)
   {!! Form::label($name, $title) !!}
 @endif
-
   {!! $text !!}
 
 @if (!empty($help))
-  <span class="help-block">{!! $help !!}</span>
+  <p><span class="help-block">{!! $help !!}</span></p>
 @endif
 
 @if (!is_null($errors) && $errors->has($name))
   <span class="text-danger">{!! $errors->first($name) !!}</span>
 @endif
+</div>
