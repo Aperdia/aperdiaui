195f661e03a7bd2cf54b430183e81a924f7addc8
Fred Delrieu <caouecs@caouecs.net>
2017-09-22 08:06:27 +0000

    feature: only bulma

--- src/views/bootstrap3/pagination.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator);
-
-if ($paginator->getLastPage() > 1) {
-    echo '<ul class="pagination">'.$presenter->render().'</ul>';
-}
