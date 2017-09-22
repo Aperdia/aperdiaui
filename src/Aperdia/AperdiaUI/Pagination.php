404c0532b52589c55d3ca07fb30249ccd7b12bf0
Fred Delrieu <caouecs@caouecs.net>
2017-09-22 08:53:36 +0000

    feature: pagination

---
+++ src/Aperdia/AperdiaUI/Pagination.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Aperdia.
+ *
+ * @copyright Aperdia
+ */
+
+namespace Aperdia\AperdiaUI;
+
+use Kilte\Pagination\Pagination;
+
+/**
+ * Pagination.
+ */
+class Pagination
+{
+    /**
+     * Pagination.
+     *
+     * @param string $url         URL of link
+     * @param int    $total       Total
+     * @param int    $currentPage Current page
+     * @param int    $nbrByPage   Number by page
+     * @param int    $neighbord   Neighbord
+     *
+     * @return string
+     */
+    public static function get(string $url, int $total, int $currentPage, int $nbrByPage = 10, int $neighbord = 4)
+    {
+        $pagination = new Pagination($total, $currentPage, $nbrByPage, $neighbord);
+        $pagination = $pagination->build();
+        
+        return $this->view('pagination', compact('pagination'));
+    }
+}
