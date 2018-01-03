4b87d1267bb390d9bcd07fbd7ee87ed4a58f3377
fdelrieu <caouecs@caouecs.net>
2018-01-03 16:35:24 +0100

    feature: modal

---
+++ src/Aperdia/AperdiaUI/Modal.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Aperdia.
+ *
+ * @copyright Aperdia
+ */
+
+namespace Aperdia\AperdiaUI;
+
+/**
+ * Modal.
+ */
+class Modal
+{
+    /**
+     * Simple modal
+     *
+     * @param string $content
+     * @param array $attributes
+     *
+     * @return string
+     */
+    public static function get(string $content, array $attributes = [])
+    {
+        return Helpers::view('modal', compact('content', 'attributes'));
+    }
+
+    /**
+     * Card Modal.
+     *
+     * @param string $content
+     * @param array $attributes
+     * @param bool $hasHeader
+     * @param string $title
+     * @param bool $hasFooter
+     * @param string $buttonSuccess
+     * @param string $buttonCancel
+     *
+     * @return string
+     */
+    public static function getCard(
+        string $content,
+        array $attributes = [],
+        bool $hasHeader = false,
+        string $title = '',
+        bool $hasFooter = false,
+        string $buttonSuccess = '',
+        string $buttonCancel = ''
+    ) {
+        return Helpers::view(
+            'modal-card',
+            compact('content', 'attributes', 'hasHeader', 'title', 'hasFooter', 'buttonSuccess', 'buttonCancel')
+        );
+    }
+}
