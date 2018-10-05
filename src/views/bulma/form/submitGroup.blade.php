c8b7ec8d7c256ebed90b246b56319d326c2650a7
caouecs <caouecs@caouecs.net>
2018-10-05 17:11:23 +0100

    feature: submit with buttons

--- src/views/bulma/form/submitGroup.blade.php
+++ src/views/bulma/form/submitGroup.blade.php
@@ -1,8 +1,8 @@
 <div class="field">
-  {!! Form::submit($options['submit_title'], \Aperdia\AperdiaUI\Helpers::addClass($attributes, 'button')) !!}
+  {!! Form::submit($options['submit_title'], \Aperdia\AperdiaUI\Helpers::addClass($attributes, 'button is-link')) !!}
 
   @if (isset($options['cancel_url']))
-    {!! link_to($options['cancel_url'], trans('form.cancel')) !!}
+    {!! link_to($options['cancel_url'], trans('form.cancel'), ['class' => 'button']) !!}
   @endif
 
   @if (isset($options['reset']) && $options['reset'] === true)
