5b049afae2dd8ceb219ac481a898872eb2dada7b
caouecs <caouecs@caouecs.net>
2018-10-07 15:06:48 +0100

    feature: submit group

--- src/views/bulma/form/submitGroup.blade.php
+++ src/views/bulma/form/submitGroup.blade.php
@@ -2,10 +2,10 @@
   {!! Form::submit($options['submit_title'], \Aperdia\AperdiaUI\Helpers::addClass($attributes, 'button is-link')) !!}
 
   @if (isset($options['cancel_url']))
-    {!! link_to($options['cancel_url'], trans('form.cancel'), ['class' => 'button']) !!}
+    {!! link_to($options['cancel_url'], trans('form.cancel'), ['class' => 'button is-white']) !!}
   @endif
 
   @if (isset($options['reset']) && $options['reset'] === true)
-    {!! Form::reset(trans('aperdiaui::ui.Reset'), ['class' => 'button is-link']) !!}
+    {!! Form::reset(trans('aperdiaui::ui.Reset'), ['class' => 'button is-white']) !!}
   @endif
 </div>
